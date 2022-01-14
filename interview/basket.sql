c9d38eb2a2d4e6d4a1fb225870e2d5a36e9b1f68
ilhami canseven <77064959+ilhamibey51@users.noreply.github.com>
2022-01-14 14:00:47 +0300

    Add files via upload

---
+++ interview/basket.sql
@@ -0,0 +1,59 @@
+-- phpMyAdmin SQL Dump
+-- version 5.1.1
+-- https://www.phpmyadmin.net/
+--
+-- Anamakine: 127.0.0.1
+-- Üretim Zamanı: 14 Oca 2022, 11:53:53
+-- Sunucu sürümü: 10.4.21-MariaDB
+-- PHP Sürümü: 8.0.12
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Veritabanı: `test`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Tablo için tablo yapısı `basket`
+--
+
+CREATE TABLE `basket` (
+  `id` int(11) NOT NULL,
+  `Name` varchar(100) NOT NULL,
+  `TotalBags` float NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dökümü yapılmış tablolar için indeksler
+--
+
+--
+-- Tablo için indeksler `basket`
+--
+ALTER TABLE `basket`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Dökümü yapılmış tablolar için AUTO_INCREMENT değeri
+--
+
+--
+-- Tablo için AUTO_INCREMENT değeri `basket`
+--
+ALTER TABLE `basket`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=31;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
---
+++ interview/index.php
@@ -0,0 +1,127 @@
+<?php session_start(); ?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Freetimers Candidate Test</title>
+</head>
+<body>
+    <?php 
+class TopspoilCalculator {
+    public $bags;
+    private $totalBags;
+    
+        function MeasurementUnit() {
+            ?>
+            <form name="MyForm" action="index.php" method="POST">
+                <input type="text" name="Name" placeholder="What is your name?" value="<?php @$_SESSION["Name"];?>" required> <br>
+            <label>Please Select Your Measurement Unit:</label> 
+                <select name="MeasurementUnit" required>
+                    <option name="MeasurementMetres">Metres</option>
+                    <option name="MeasurementFeet">Feet</option>
+                    <option name="MeasurementYards">Yards</option>
+                </select>
+            <?php
+        }
+        function DepthMeasurementUnit() {
+            ?><label>Please Select Your Depth Measurement Unit:</label> 
+                <select name="DepthMeasurementUnit" required>
+                    <option name="DepthMeasurementcentimetres">Centimetres</option>
+                    <option name="MeasurementInches">Inches</option>
+                </select>
+            <?php
+
+        }
+        function SetDimensions() {  
+            ?> <label>Please Enter Your Dimensions:</label> 
+               <input name="Dimensionwidth" type="number" placeholder="Enter The width please" required></input>
+               <input name="Dimensionlength" type="number" placeholder="Enter The length please" required></input>
+               <input name="Dimensiondepth" type="number" placeholder="Enter The depth please" required></input>
+               <input name="myButton" type="submit" value="Submit"></input>
+               
+               
+            <?php
+
+        }
+        function CalculateNumberOfBags() {
+            
+        if(@$_POST["MeasurementUnit"] == "Feet"):
+        $feetToMeterWidth = 0;
+        $feetToMeterHeight = 0;
+        $feetToMeterWidth =  $_POST["Dimensionwidth"] / 3.2808;
+        $feetToMeterHeight =  $_POST["Dimensionlength"] / 3.2808;
+        $metresSquare = $feetToMeterHeight * $feetToMeterWidth;
+        $X = $metresSquare * 0.025; 
+        $Y = $X * 1.4;
+        $this->bags = ceil($Y);
+        echo "You Need"." ".$this->bags." "."Bags of Top Soil For this one <br>"; 
+
+        
+
+        elseif(@$_POST["MeasurementUnit"] == "Yards"):
+            $YardToMeterWidth = 0;
+            $YardToMeterHeight = 0;
+            $YardToMeterWidth =  $_POST["Dimensionwidth"] / 1.0936;
+            $YardToMeterHeight =  $_POST["Dimensionlength"] / 1.0936;
+            $metresSquare = $YardToMeterHeight * $YardToMeterWidth;
+            $X = $metresSquare * 0.025; 
+            $Y = $X * 1.4;
+            $this->bags = ceil($Y);
+            echo "You Need"." ".$this->bags." "."Bags of Top Soil For this one <br>"; 
+
+        elseif(@$_POST["MeasurementUnit"] == "Metres"):
+            $MeterWidth = 0;
+            $MeterHeight = 0;
+            $MeterWidth =  $_POST["Dimensionwidth"];
+            $MeterHeight =  $_POST["Dimensionlength"];
+            $metresSquare = $MeterHeight * $MeterWidth;
+            $X = $metresSquare * 0.025; 
+            $Y = $X * 1.4;
+            $this->bags = ceil($Y);
+            echo "You Need"." ".$this->bags." "."Bags of Top Soil For this one <br>"; 
+            
+            
+        endif;
+       
+            
+        }
+         /// 
+    function addToBasket($name) {
+        $servername = "localhost";
+        $username = "root";
+        $password = "";
+        
+
+
+        $conn = new PDO("mysql:host=$servername;dbname=test", $username, $password);
+        
+        $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+
+        @$sql = "insert into basket(Name,TotalBags) values(?,?)";
+        @$inserter = $conn->prepare($sql);
+        @$inserter->execute(array($name,$this->bags));
+
+        @$sqlSum = $conn->prepare("select SUM(TotalBags) as value from basket WHERE Name=?");
+        @$sqlSum->execute(array($name));
+        $row = $sqlSum->fetch(PDO::FETCH_ASSOC);
+        echo "You Need". " ". $row["value"]. " "."Bags of Top soil for all.";
+    }
+}
+
+
+
+@$_SESSION["Name"] = $_POST["Name"];
+
+$MyClass = new TopspoilCalculator();
+$MyMeasurementFunction = $MyClass->MeasurementUnit(); echo "<br>";
+$MyDepthMeasurementFunction = $MyClass->DepthMeasurementUnit(); echo "<br>";
+$MySetDimensionsFunction = $MyClass->SetDimensions(); echo "<br>";
+$myCalculateFunc = $MyClass->CalculateNumberOfBags();
+$MyAddTobasketFunction =$MyClass->addToBasket(@$_SESSION["Name"]); echo "<br>";
+?>
+</form>  
+</body>
+</html>
